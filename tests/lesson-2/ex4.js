b775f15b86aebe996a3987fcd89bd325768d933d
nguyet.pham <phamminhnguyet0508@gmail.com>
2025-10-21 00:20:29 +0700

    update javascript exercises

---
+++ tests/lesson-2/ex4.js
@@ -0,0 +1,6 @@
+const height = 153;
+let fractionalHeight = height - 100;
+let idealWeight = (fractionalHeight * 9) / 10;
+let maxWeight = fractionalHeight;
+let minWeight = (fractionalHeight * 8) / 10;
+console.log("My Ideal Weight is " + idealWeight + "kg. My Maximum Weight is " + maxWeight + "kg. My Minimum Weight is " + minWeight + "kg.")
